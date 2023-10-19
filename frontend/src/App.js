0ff3bf8315e664e734fdba8dca24dafd1541832d
Suhuai <suhuaichen2024@u.northwestern.edu>
2023-10-19 11:11:02 +0200

    Finished Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <h1>Welcome to EShop</h1>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+    
   )
 }
 
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Card, CardBody, CardText, CardTitle } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
+
+const Product = ({product}) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+        <Link to={`/product/${product._id}`}>
+            <Card.Img src={product.image} variant='top'/>
+        </Link>
+
+        <CardBody>
+            <Link to={`/product/${product._id}`}>
+                <CardTitle as='div' className='product-title'>
+                    <strong>{product.name}</strong>
+                </CardTitle>
+            </Link>
+
+            <Card.Text as='div'>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </Card.Text>
+
+            <CardText as="h3">
+                ${product.price}
+            </CardText>
+        </CardBody>
+    </Card>
+  )
+}
+
+export default Product
